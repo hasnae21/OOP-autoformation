e3e355629e195044cd30c1b5940401cfa5074865
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-09-30 12:24:07 +0100

    save

---
+++ classe_et_instance_de_classe/index.php
@@ -0,0 +1,14 @@
+<?php
+    class Voiture{
+        private $couleur ="rouge";
+        public $vitesse =0;
+
+    }
+
+    
+    //$suv = new Voiture();
+    //$limousine=new Voiture();
+    
+    $berline = new Voiture(); //objet
+    echo $berline-> couleur; 
+?>
