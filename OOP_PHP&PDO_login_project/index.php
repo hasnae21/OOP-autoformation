a7e2cfd2acded660155474e993ccafc4dc0488bd
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-03 09:16:28 +0100

    save

--- OOP_PHP&PDO_login_project/index.php
+++ OOP_PHP&PDO_login_project/index.php
@@ -6,26 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login page</title>
 </head>
-<body>
-<form action="includes/signup.inc.php" method="POST">
+<body style="display:flex; margin:10%;">
 
-<input type="text" name="uid" placeholder="Username">
-<input type="password" name="pwd" placeholder="Password"> 
-<input type="password" name="pwdrepeat" placeholder="Password" >
-<input type="email" name="email" placeholder="E-mail">
-<br>
-<button type="submit">SIGN UP</button>
-
-</form>    
-
-<form action="includes/login.inc.php" method="POST">
-
-<input type="text" name="uid" placeholder="Username">
-<input type="password" name="pwd" placeholder="Password">
-<br>
-<button type="submit">LOGIN</button>
-
-</form>
-
+<section style="padding: 22px;">
+    <form action="includes/signup.inc.php" method="POST">
+        
+        <input type="text" name="uid" placeholder="Username">
+        <br>
+        <input type="password" name="pwd" placeholder="Password">
+        <br> 
+        <input type="password" name="pwdrepeat" placeholder="Password" >
+        <br>
+        <input type="email" name="email" placeholder="E-mail">
+        <br>
+        <button type="submit">SIGN UP</button>
+    </form> 
+</section>
+<section>
+    <form action="includes/login.inc.php" method="POST">
+        
+        <input type="text" name="uid" placeholder="Username">
+        <br>
+        <input type="password" name="pwd" placeholder="Password">
+        <br>
+        <button type="submit">LOGIN</button>
+    </form>
+</section>
+    
 </body>
 </html>
