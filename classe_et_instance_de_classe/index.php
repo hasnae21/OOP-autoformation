ee07d1c7cd58b247eb53956af2b3147e1c4f3fe4
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 15:19:05 +0100

    save

--- classe_et_instance_de_classe/index.php
+++ classe_et_instance_de_classe/index.php
@@ -42,6 +42,7 @@
       $apple = new Fruit();
       $apple->set_name('Apple');
       $apple->set_color('Red');
+      echo "<br>";
       echo "Name: " . $apple->get_name();
       echo "<br>";
       echo "Color: " . $apple->get_color();  //Name: Apple 
