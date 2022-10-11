cedb9ffd6d507470197a6c0c64915d5dbb7df97f
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 15:16:58 +0100

    save

--- classe_et_instance_de_classe/index.php
+++ classe_et_instance_de_classe/index.php
@@ -1,20 +1,87 @@
 <?php
     class Voiture{
+
         private $couleur ="rouge";
         public $vitesse =0;
+
         public function afficherCouleur(){
             return $this-> couleur;
-            
         }
         public function repeindre($co){
             $this-> couleur= $co;
         }
     }
-
     
     //$suv = new Voiture();
-    //$limousine=new Voiture();
+    //$limousine = new Voiture();
     
     $berline = new Voiture(); //objet
     echo $berline-> afficherCouleur();  //eror qd couleur est private donc on utilise function     
-?>
+
+//class and methodes 
+    class Fruit {
+        // Properties
+        public $name;
+        public $color;
+      
+        // Methods
+        function set_name($name) {
+          $this->name = $name;
+        }
+        function get_name() {
+          return $this->name;
+        }
+        function set_color($color) {
+          $this->color = $color;
+        }
+        function get_color() {
+          return $this->color;
+        }
+      }
+      
+      $apple = new Fruit();
+      $apple->set_name('Apple');
+      $apple->set_color('Red');
+      echo "Name: " . $apple->get_name();
+      echo "<br>";
+      echo "Color: " . $apple->get_color();  //Name: Apple 
+                                             //Color: Red
+
+
+//change the value of the $name property
+
+//////1_choix:
+class Fruit {
+    public $name;
+    function set_name($name) {
+      $this->name = $name;
+    }
+  }
+  $apple = new Fruit();
+  $apple->set_name("Apple");
+
+//////2_choix:
+class Fruit {
+    public $name;
+  }
+  $apple = new Fruit();
+  $apple->name = "Apple";
+
+///instanceof to know if an object is an instance of a class or not /true or false
+
+    class Fruit {
+        // Properties
+        public $name;
+        public $color;
+      
+        // Methods
+        function set_name($name) {
+          $this->name = $name;
+        }
+        function get_name() {
+          return $this->name;
+        }
+      }
+      
+      $apple = new Fruit();
+      var_dump($apple instanceof Fruit);   //bool(true)
