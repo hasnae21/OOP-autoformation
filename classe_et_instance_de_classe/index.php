e8ff9d7f5f267295117e735ccb3cbdbb2e2dd450
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 15:22:55 +0100

    s

--- classe_et_instance_de_classe/index.php
+++ classe_et_instance_de_classe/index.php
@@ -45,8 +45,11 @@
       echo "<br>";
       echo "Name: " . $apple->get_name();
       echo "<br>";
-      echo "Color: " . $apple->get_color();  //Name: Apple 
-                                             //Color: Red
+      echo "Color: " . $apple->get_color();
+      echo "<br>";
+      
+        //Name: Apple 
+        //Color: Red
 
 
 //change the value of the $name property
