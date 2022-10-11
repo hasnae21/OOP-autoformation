d9f86dcbd51dddf7007c42fd9eb36e89328eec8f
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 15:43:23 +0100

    s

--- constractor/index.php
+++ constractor/index.php
@@ -14,3 +14,25 @@ class Fruit {
 
 $lkouk = new Fruit("lkouk"); //no need of set_name methode
 echo $lkouk->get_name();
+
+
+class Fruits {
+    public $name;
+    public $color;
+  
+    function __construct($name, $color) {
+      $this->name = $name; 
+      $this->color = $color; 
+    }
+    function get_name() {
+      return $this->name;
+    }
+    function get_color() {
+      return $this->color;
+    }
+  }
+  
+  $apple = new Fruits("Apple", "red");
+  echo $apple->get_name();
+  echo "<br>";
+  echo $apple->get_color();
